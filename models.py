551026c6367c2cc7e435ff1c22e2dcd374bdba42
matheusafonsouza <matheusafonsouza@gmail.com>
2022-04-18 00:39:54 -0300

    category model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
+from unicodedata import name
 from common.models import BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Item(BaseModel):
@@ -8,3 +10,13 @@ class Item(BaseModel):
     title = Column(String)
     description = Column(String)
     status = Column(String)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship("Category", back_populates="items")
+
+
+class Category(BaseModel):
+    __tablename__ = "category"
+
+    name = Column(String)
+    description = Column(String)
+    items = relationship("Item", back_populates="category")
